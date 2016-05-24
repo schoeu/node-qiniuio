c3bcc3b41afd8d40455203aad4b5bb64dfa68135
schoeu <quanyou1013@gmail.com>
2016-05-24 21:19:22 +0800

    update chenge file structure

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * node-qiniuio入口文件
+ * **/
+
 var getFileInfo = require('./lib/qiniu_getfileinfo');
 var uploadCb = require('./lib/qiniu_upload_cb');
 var download = require('./lib/qiniu_download');
@@ -5,13 +9,6 @@ var upload = require('./lib/quniu_upload');
 var qiniu_conf = require('./lib/qiniu_config.json');
 
 module.exports = {
-    // @example
-    // qiniu.getFileInfo('css/common/common.css', function (err, info) {
-    //   if (err) {
-    //     throw err;
-    //   }
-    //   console.log(info);
-    // });
     getFileInfo: getFileInfo,
     upload: upload,
     uploadCb: uploadCb,
